127534bf7f2ddb148df08ce82e72e555d1779967
Corina Udrescu <corinutz@gmail.com>
2024-06-22 14:31:27 +0200

    load first page of pokemons

--- src/PokemonsList.jsx
+++ src/PokemonsList.jsx
@@ -1,5 +1,40 @@
+import { useState } from "react";
+import { useEffect } from "react";
+
+const PAGE_SIZE = 6;
+
+const fetchPokemonPage = async (offset) => {
+  const response = await fetch(
+    `https://pokeapi.co/api/v2/pokemon?limit=${PAGE_SIZE}&offset=${offset}`
+  );
+  const data = await response.json();
+  console.log(data);
+  return data.results;
+};
+
 const PokemonsList = () => {
-  return <div>Here be pokemons</div>;
+  const [pokemons, setPokemons] = useState([]);
+  useEffect(() => {
+    fetchPokemonPage().then((firstPageOfPokemons) =>
+      setPokemons(firstPageOfPokemons)
+    );
+  }, []);
+  return (
+    <div style={{ maxWidth: "500px", margin: "auto" }}>
+      {pokemons.map((pokemon) => (
+        <div
+          key={pokemon.name}
+          style={{
+            border: "1px solid lightgray",
+            padding: "5px",
+            margin: "5px",
+          }}
+        >
+          <h3>{pokemon.name}</h3>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default PokemonsList;
